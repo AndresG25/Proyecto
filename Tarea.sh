a846d6120dda0fe73d9d599ffd9adcfb59b9c796
Andres <andres25@utp.edu.co>
2022-05-09 14:44:16 -0500

    Nuevo commit

---
+++ Tarea.sh
@@ -0,0 +1,7 @@
+cd C:/Users/USUARIO/Desktop/MAESTRIA/Tesis-Maestria/Shiny_Reactiva/Prueba_Reactiva/Proyecto
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Nuevo commit"
+git push origin main
